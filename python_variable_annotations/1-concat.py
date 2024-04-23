939c838d70adf904cad4c06a34df89c58f424712
git-alexis <alex59.billemont@gmail.com>
2024-04-23 20:43:16 +0200

    Add type annotations to the concat function that concatenates two strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function add that takes a string a and a string b as arguments
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of two strings"""
+    return str1 + str2
