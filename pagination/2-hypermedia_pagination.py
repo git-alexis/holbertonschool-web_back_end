2e185f11b996a5e3aa678b1d7ae414c01afcec25
git-alexis <alex59.billemont@gmail.com>
2024-05-03 23:05:04 +0200

    invers too line: place start affectation before if statement

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -38,12 +38,12 @@ class Server:
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
 
+        start, end = index_range(page, page_size)
         data = self.dataset()
 
         if start > len(data):
             return []
 
-        start, end = index_range(page, page_size)
         return data[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
