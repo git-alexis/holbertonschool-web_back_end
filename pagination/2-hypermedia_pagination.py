64c7451c0994fcc01ed10e07af9c3f0aaf272219
git-alexis <alex59.billemont@gmail.com>
2024-05-03 22:44:19 +0200

    Add hypermedia pagination method to take data pagination size in dictionnary

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+""" Simple helper function """
+
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+    Return a tuple of size two containing a start index and an end index
+    """
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Get the correct page of the dataset in a list """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+
+        data = self.dataset()
+
+        if start > len(data):
+            return []
+
+        start, end = index_range(page, page_size)
+        return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """ Get the correct page of the dataset in a dictionary """
+
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
