95fb110cf2efa1d6022785d684206365dfc02640
git-alexis <alex59.billemont@gmail.com>
2024-05-08 16:56:09 +0200

    fix ES6 syntax

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const employeesDepartment = {
-    [departmentName]: employees
+    departmentName: employees
   };
 
   return employeesDepartment;
