e7e56b768946f60956d1ba3c03b0b06a6904b0b2
git-alexis <alex59.billemont@gmail.com>
2024-05-17 11:01:54 +0200

    Return a Promise using this prototype function getResponseFromAPI()

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,12 +1,5 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true;
-      if (success) {
-        resolve(success);
-      } else {
-        reject(success);
-      }
-    }, 2000);
+  return new Promise((resolve) => {
+    resolve('API response');
   });
 }
