6dca095da95da5e1b1b6c340ea14a7f7a0346477
git-alexis <alex59.billemont@gmail.com>
2024-05-17 19:41:27 +0200

    Knowing that the functions in utils.js return promises, use the prototype to collectively resolve all promises and log body firstName lastName to the console

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+      const [photo, user] = values;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline')
