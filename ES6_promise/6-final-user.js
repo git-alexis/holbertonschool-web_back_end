ce025145072fcf55e8444efa084cfce2342e8270
git-alexis <alex59.billemont@gmail.com>
2024-05-17 19:56:44 +0200

    return an array with the following structure [ { status: status_of_the_promise, value: value or error returned by the Promise }, ... ] when the promises are all settled

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName)
+  const photo = uploadPhoto(fileName)
+
+  const results = await Promise.allSettled([signUpPromise, uploadPhotoPromise]);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+  }));
+}
