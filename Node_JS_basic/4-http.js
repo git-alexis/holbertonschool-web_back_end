502a823b2119d54885c7a160600f69e29c2df441
git-alexis <alex59.billemont@gmail.com>
2024-09-22 22:41:53 +0200

    fix eslint

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain'); 
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
