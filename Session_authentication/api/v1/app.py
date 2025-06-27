9792b5cab55429a426e37f2a5d965546cab3acdd
git-alexis <alex59.billemont@gmail.com>
2025-06-27 23:32:35 +0200

    Add all functions and routes for new module and view for Session Authentication

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
@@ -14,9 +15,9 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth_type = getenv("AUTH_TYPE")
-if auth_type == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
+if auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -29,12 +30,13 @@ def before_request_func():
         return
 
     exempt_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                    '/api/v1/forbidden/']
+                    '/api/v1/forbidden/', '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, exempt_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
 
     request.current_user = auth.current_user(request)
