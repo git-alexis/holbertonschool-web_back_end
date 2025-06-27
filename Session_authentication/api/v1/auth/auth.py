ac34b6d851d3e8ebb0d6915949be53974f7c8299
git-alexis <alex59.billemont@gmail.com>
2025-06-28 00:00:26 +0200

    Add session_cookie function

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -43,3 +43,15 @@ class Auth():
         """ Retrieves the current user from the request.
         """
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns a cookie value from a request
+        """
+        if request is None:
+            return None
+
+        session_name = os.getenv('SESSION_NAME')
+        if session_name is None:
+            return None
+
+        return request.cookies.get(session_name, None)
