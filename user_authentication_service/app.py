ffee920c1b522beea3c79e46837711598f7f6acc
git-alexis <alex59.billemont@gmail.com>
2025-06-27 17:12:30 +0200

    Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,10 @@
 """App module
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -12,6 +15,20 @@ def welcome():
     """Endpoint returning a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users",  methods=['POST'], strict_slashes=False)
+def register_user():
+    """Endpoint to register a user.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
