8a745dd3147d633fa53a075e57c2833cb6fa12f7
git-alexis <alex59.billemont@gmail.com>
2025-06-27 16:21:01 +0200

    Find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,6 +4,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -36,3 +38,18 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+            """ 
+            Take in arbitrary keyword arguments and
+            return the first row found in the users
+            """
+            try:
+                result = self._session.query(User).filter_by(**kwargs).first()
+                if result is None:
+                    raise NoResultFound
+                return result
+            except NoResultFound:
+                raise NoResultFound("No user found.")
+            except InvalidRequestError:
+                raise InvalidRequestError("Invalid query parameters.")
