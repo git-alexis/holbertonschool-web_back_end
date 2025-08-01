f6ccfb64de922a656277a9df1ba1cfc967466901
git-alexis <alex59.billemont@gmail.com>
2025-08-01 16:29:08 +0200

    Change the default date for the split of a band

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Lists all bands with Glam rock as their main style
 -- Ranked by their longevity
-SELECT band_name, IFNULL(split, YEAR(CURDATE())) - formed AS lifespan FROM metal_bands
+SELECT band_name, IFNULL(split, 2024) - formed AS lifespan FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
