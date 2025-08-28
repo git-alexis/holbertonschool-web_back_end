db641d7da877bb5fc9c70fbb3f15f90324df725b
git-alexis <alex59.billemont@gmail.com>
2025-08-28 19:15:04 +0200

    Basic test with Mocha and Node assertion library

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,1 +1,13 @@
+/**
+ * calculateNumber - round a and b and return the calcul of it
+ * Accepts two numbers a and b
+ * Returns the calcul result after rounding them
+ */
 
+function calculateNumber(a, b) {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+  return roundedA + roundedB;
+}
+
+module.exports = calculateNumber;
--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,1 +1,51 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
 
+describe('calculateNumber', function () {
+
+  it('should return 7 when a=2, b=5', function () {
+    assert.strictEqual(calculateNumber(2, 5), 7);
+  });
+  it('should return 7 when a=2, b=4.5', function () {
+    assert.strictEqual(calculateNumber(2, 4.5), 7);
+  });
+  it('should return 7 when a=1.2, b=5.7', function () {
+    assert.strictEqual(calculateNumber(1.2, 5.7), 7);
+  });
+  it('should return 7 when a=1.8, b=4.5', function () {
+    assert.strictEqual(calculateNumber(1.8, 4.5), 7);
+  });
+
+  it('should return 7 when a=12, b=-5', function () {
+    assert.strictEqual(calculateNumber(12, -5), 7);
+  });
+  it('should return -7 when a=2, b=-9', function () {
+    assert.strictEqual(calculateNumber(2, -9), -7);
+  });
+  it('should return 7 when a=10.6, b=-4', function () {
+    assert.strictEqual(calculateNumber(10.6, -4), 7);
+  });
+  it('should return -7 when a=2, b=-8.6', function () {
+    assert.strictEqual(calculateNumber(2, -8.6), -7);
+  });
+
+  it('should return 7 when a=-2.9, b=9.5', function () {
+    assert.strictEqual(calculateNumber(-2.9, 9.5), 7);
+  });
+  it('should return -7 when a=-11.4, b=4.3', function () {
+    assert.strictEqual(calculateNumber(-11.4, 4.3), -7);
+  });
+
+  it('should return -7 when a=-2, b=-5', function () {
+    assert.strictEqual(calculateNumber(-2, -5), -7);
+  });
+  it('should return -7 when a=-3, b=-4.5', function () {
+    assert.strictEqual(calculateNumber(-3, -4.5), -7);
+  });
+  it('should return -7 when a=-1.2, b=-5.7', function () {
+    assert.strictEqual(calculateNumber(-1.2, -5.7), -7);
+  });
+  it('should return -7 when a=-2.9, b=-4.5', function () {
+    assert.strictEqual(calculateNumber(-2.9, -4.5), -7);
+  });
+});
