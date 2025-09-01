da342b5ae1f203229e123b617e1e9b5a688138d9
git-alexis <alex59.billemont@gmail.com>
2025-09-01 11:44:14 +0200

    Update doc of fonction

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,7 +1,9 @@
 /**
- * calculateNumber - round a and b and return the calcul of it
- * Accepts two numbers a and b
- * Returns the calcul result after rounding them
+ * calculateNumber - round a and b and return the sum of it
+ *
+ * @param {number} a - The first number to round.
+ * @param {number} b - The second number to round.
+ * @returns {number} The sum of the two rounded numbers.
  */
 
 function calculateNumber(a, b) {
