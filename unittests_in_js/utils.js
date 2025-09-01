79f5b18f2083d51e74ec01d09bf6ee16f2086bb2
git-alexis <alex59.billemont@gmail.com>
2025-09-01 14:25:18 +0200

    Add test with Spies on Utils.calculateNumber property

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,29 @@
+/**
+ * calculateNumber - round a and b and return the calcul of it
+ *
+ * @param {string} type - The type of operation to do.
+ * @param {number} a - The first number to round.
+ * @param {number} b - The second number to round.
+ * @returns {number} The calcul result or Error if type = DIVIDE and b = 0.
+ */
+
+function calculateNumber(type, a, b) {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  if (type == "SUM")
+    return roundedA + roundedB;
+  else if (type == "SUBTRACT")
+    return roundedA - roundedB;
+  else if (type == "DIVIDE")
+    if (roundedB == 0)
+      return "Error";
+    else
+      return roundedA / roundedB;
+}
+
+const Utils = {
+  calculateNumber,
+};
+
+module.exports = Utils;
