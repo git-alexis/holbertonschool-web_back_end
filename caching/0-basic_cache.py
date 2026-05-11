63071a94e97ccaa8e80ac0b4121e393709bdd99e
git-alexis <alex59.billemont@gmail.com>
2026-05-11 15:51:14 +0200

    Création de la classe BasicCache irritant de BaseCaching et avec deux méthodes, put et get, qui mettent en cache ou récupère un élément

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Basic caching module """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class that inherits from BaseCaching """
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
---
+++ caching/README.md
@@ -0,0 +1,10 @@
+# Caching
+
+This project introduces Caching.
+
+## Tasks
+- Basic dictionary
+- FIFO caching
+- LIFO Caching
+- LRU Caching
+- MRU Caching
