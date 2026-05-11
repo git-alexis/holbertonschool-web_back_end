dce1863169066a7ead0eadaa2830309c23e50c62
git-alexis <alex59.billemont@gmail.com>
2026-05-11 18:54:01 +0200

    Création de la classe LRUCache avec l'utiliation de l'algorithme LRU pour la méthode put

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" LRU caching module """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache class that inherits from BaseCaching """
+
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.keys_order = []
+
+    def put(self, key, item):
+        """ Add an item to the cache using LRU """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+            if key in self.keys_order:
+                self.keys_order.remove(key)
+            self.keys_order.append(key)
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = self.keys_order.pop(0)
+                del self.cache_data[first_key]
+                print("DISCARD: {}".format(first_key))
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
