2f0a5dfd484ea6c3cd053226e8e870e28e5daa20
git-alexis <alex59.billemont@gmail.com>
2026-05-12 16:47:02 +0200

    Création de la fonction filter_datum qui renvoie un message sous forme masquée en utilisant une regex dans re.sub()

---
+++ personal_data/README.md
@@ -0,0 +1,12 @@
+# Personal data
+
+This project introduces Personal data.
+
+## Tasks
+- Regex-ing
+- Log formatter
+- Create logger
+- Connect to secure database
+- Read and filter data
+- Encrypting passwords
+- Check valid password
---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Filtered logger module """
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """ Return the log message with obfuscated fields """
+    return re.sub(
+        fr"({'|'.join(fields)})=[^{separator}]*",
+        lambda mes: f"{mes.group(1)}={redaction}",
+        message
+    )
