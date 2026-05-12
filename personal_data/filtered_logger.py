e886d6588a22b3c7176bd21c5e82187def9583dd
git-alexis <alex59.billemont@gmail.com>
2026-05-12 17:19:32 +0200

    Ajout de la classe RedactingFormatter avec rédaction des champs sensibles

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Filtered logger module """
 
+import logging
 import re
 from typing import List
 
@@ -13,3 +14,23 @@ def filter_datum(fields: List[str], redaction: str,
         lambda mes: f"{mes.group(1)}={redaction}",
         message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        """ Initialize the formatter with fields to redact """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Return the formatted log record with redacted fields """
+        return filter_datum(
+            self.fields, self.REDACTION,
+            super().format(record), self.SEPARATOR
+        )
