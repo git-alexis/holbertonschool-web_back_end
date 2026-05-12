c864831eb18e7f57554aad8aac114d530a3025f5
git-alexis <alex59.billemont@gmail.com>
2026-05-12 17:22:49 +0200

    Suppression d'une annotation de type

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -23,7 +23,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields: List[str]):
         """ Initialize the formatter with fields to redact """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
