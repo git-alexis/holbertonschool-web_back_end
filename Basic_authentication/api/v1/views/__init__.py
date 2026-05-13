9b1ac4d41fd3d20ff374b513f2afe5b5493b0ec9
git-alexis <alex59.billemont@gmail.com>
2026-05-13 16:31:19 +0200

    Annulation du déplacement des import

--- Basic_authentication/api/v1/views/__init__.py
+++ Basic_authentication/api/v1/views/__init__.py
@@ -2,9 +2,10 @@
 """ DocDocDocDocDocDoc
 """
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.users import *
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *  # noqa
+from api.v1.views.users import *  # noqa
 
 User.load_from_file()
