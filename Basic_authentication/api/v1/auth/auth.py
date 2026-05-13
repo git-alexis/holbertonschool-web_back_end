217b8fb4dccd4e0b4eafaf52bffe9fa671a0ffba
git-alexis <alex59.billemont@gmail.com>
2026-05-13 17:06:28 +0200

    Ajout de la validation des requêtes avec la classe Auth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -26,7 +26,13 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Return authorization header """
-        return None
+        if request is None:
+            return None
+
+        if request.headers.get('Authorization') is None:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Return current user """
