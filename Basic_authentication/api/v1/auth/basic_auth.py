310f745f34cb17692c331886b142c33b33eaedd5
git-alexis <alex59.billemont@gmail.com>
2026-05-13 18:06:36 +0200

    Ajout d'un try/except sur User.search pour gérer le cas d'une DB sans User

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -67,7 +67,10 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        users = User.search({"email": user_email})
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
 
         if len(users) == 0:
             return None
