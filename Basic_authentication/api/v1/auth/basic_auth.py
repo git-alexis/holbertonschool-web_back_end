d4f82dfb866b2e7750be2613d0b49c6f601d8b1c
git-alexis <alex59.billemont@gmail.com>
2026-05-13 17:33:33 +0200

    Correction pycodestyle pour une ligne trop longue

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -23,7 +23,8 @@ class BasicAuth(Auth):
         return authorization_header[6:]
 
     def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str) -> str:
+                                           base64_authorization_header: str
+                                           ) -> str:
         """ Decode a Base64 authorization header """
         if base64_authorization_header is None:
             return None
