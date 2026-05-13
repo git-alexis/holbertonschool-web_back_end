e4840eccfc8aeef95b3380beff68604658619498
git-alexis <alex59.billemont@gmail.com>
2026-05-13 16:55:38 +0200

    Ajout de la gestion des routes exclues de l'authentification dans require_auth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,8 +9,20 @@ class Auth:
     """ Manage API authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Return False for now """
-        return False
+        """ Check if the path requires authentication """
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Return authorization header """
