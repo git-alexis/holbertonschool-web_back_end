97f6c0272701c2dbb0dea1c41e665108543160c4
git-alexis <alex59.billemont@gmail.com>
2026-05-13 16:37:42 +0200

    Ajout du handler pour l'erreur Forbidden statut 403

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized():
     """ Raise a 401 error """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ Raise a 403 error """
+    abort(403)
