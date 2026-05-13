ea1408ab91335bd276a224c634912457fb2c4279
git-alexis <alex59.billemont@gmail.com>
2026-05-13 18:17:21 +0200

    Ajout de la récupération de l'utilisateur courant dans la classe BasicAuth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -80,3 +80,26 @@ class BasicAuth(Auth):
                 return user
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Retrieve the User instance for a request """
+        auth_header = self.authorization_header(request)
+
+        base64_auth = self.extract_base64_authorization_header(
+            auth_header
+        )
+
+        decoded_auth = self.decode_base64_authorization_header(
+            base64_auth
+        )
+
+        user_email, user_password = self.extract_user_credentials(
+            decoded_auth
+        )
+
+        user = self.user_object_from_credentials(
+            user_email,
+            user_password
+        )
+
+        return user
